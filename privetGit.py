60455a45aca39d95844dabee1d605d26020959c6
Alex <alexnioblack@gmail.com>
2024-08-05 15:47:13 +0700

    f

---
+++ privetGit.py
@@ -0,0 +1,32 @@
+from aiogram import Bot, Dispatcher
+from aiogram.filters import BaseFilter
+from aiogram.types import Message
+from data_base import BOT_TOKEN, My_id
+
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
+# полученный у @BotFather
+
+# Создаем объекты бота и диспетчера
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+admin_ids: list[int] = [My_id]
+
+
+class IsAdmin(BaseFilter):
+    def __init__(self, admin_ids: list[int]) -> None:
+        self.admin_ids = admin_ids
+    async def __call__(self, message: Message) -> bool:
+        return message.from_user.id in self.admin_ids
+
+# Этот хэндлер будет срабатывать, если апдейт от админа
+@dp.message(IsAdmin(admin_ids))
+async def answer_if_admins_update(message: Message):
+    await message.answer(text='Вы админ')
+# Этот хэндлер будет срабатывать, если апдейт не от админа
+@dp.message()
+async def answer_if_not_admins_update(message: Message):
+    await message.answer(text='Вы не админ')
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
